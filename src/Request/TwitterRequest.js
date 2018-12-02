0eb3db1728e8f6b45e41613e717a668cb3a28787
igor.orsag <igor.orsag@gmail.com>
2018-12-02 23:07:06 +0100

    FETCHED_TWEETS_CONUT

--- src/Request/TwitterRequest.js
+++ src/Request/TwitterRequest.js
@@ -1,13 +1,17 @@
 import { ExampleResponse } from './ExampleResponse';
 import Defaults from 'superagent-defaults';
 
+const FETCHED_TWEETS_CONUT = 50;
+
 export const fetchTweets = params => {
   const superagent = getSuperagent();
   // https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
 
   // return superagent.get(`search/tweets.json?q=${params.userName}`)
   return superagent.get(
-    `1.1/statuses/user_timeline.json?screen_name=${params.userName}&count=5`
+    `1.1/statuses/user_timeline.json?screen_name=${
+      params.userName
+    }&count=${FETCHED_TWEETS_CONUT}`
   );
 };
 
@@ -15,7 +19,7 @@ export const fetchUsers = params => {
   const superagent = getSuperagent();
   console.log('users');
 
-  return superagent.get(`1.1/users/search.json?q=peterson&page=1&count=5`);
+  return superagent.get(`1.1/users/search.json?q=peterson&page=1&count=50`);
 };
 
 export const fetchFakeTweets = params => {
