78d494a7996256febeb2672b9932d7aa2c0df7b0
igor.orsag <igor.orsag@gmail.com>
2019-01-11 17:36:45 +0100

    hooks rerenders

--- src/Tweet/Search.js
+++ src/Tweet/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
@@ -10,23 +10,23 @@ const mapDispatchToProps = { setTweets };
 
 const Search = ({ location, history, userName, setTweets }) => {
   const [formState, setFormState] = useState({ userName: userName || '' });
-  return (
-    <SearchForm
-      state={formState}
-      handleSubmit={event => {
-        event.preventDefault();
-        setTweets(formState);
-        history.push({
-          ...location,
-          search: `?userName=${formState.userName}`
-        });
-      }}
-      handleChange={event => {
-        const { name, value } = event.target;
-        setFormState({ ...formState, [name]: value });
-      }}
-    />
+  const submitHandler = useCallback(
+    event => {
+      event.preventDefault();
+      setTweets(formState);
+      history.push({
+        ...location,
+        search: `?userName=${formState.userName}`
+      });
+    },
+    [formState, history]
   );
+
+  const changeHanlder = useCallback(event => {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  }, [])
+  return <SearchForm state={formState} handleSubmit={submitHandler} handleChange={changeHanlder} />;
 };
 const mapStateToProps = (state, { location, history }) => {
   const userName = getUserName(location.search);
